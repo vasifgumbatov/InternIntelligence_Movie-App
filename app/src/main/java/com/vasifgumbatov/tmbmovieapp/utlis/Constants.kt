5caeaaf9d7e7552531dee20f19efda3c0acb4434
vasifgumbatov <gumbatovvasif4@gmail.com>
2024-12-21 15:38:15 +0400

    fixed problems

--- app/src/main/java/com/vasifgumbatov/tmbmovieapp/utlis/Constants.kt
+++ app/src/main/java/com/vasifgumbatov/tmbmovieapp/utlis/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "26e9152e96e1919005d1ffd2765a37f0"
+        const val API_KEY = ""
         const val BASE_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w780/"
         const val BASE_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
         const val nowPlayingAllListScreen = "nowPlayingAllListScreen"
